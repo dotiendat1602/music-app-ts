454015b107d40ffb6a164be8db31be5ab749591c
Do Tien Dat <dotiendat711@gmail.com>
2024-09-21 14:06:25 +0700

    Nhúng file tĩnh

--- index.ts
+++ index.ts
@@ -13,6 +13,8 @@ const port: number | string = process.env.PORT || 3000;
 app.set("views", "./views");
 app.set("view engine", "pug"); 
 
+app.use(express.static("public"));
+
 routesClient(app);
 
 app.listen(port, () => {

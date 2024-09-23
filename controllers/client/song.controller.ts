6d8487d970608e92d07cfb00453819e1b9d7dd63
Do Tien Dat <dotiendat711@gmail.com>
2024-09-23 22:15:33 +0700

    Làm trang chi tiết bài hát

--- controllers/client/song.controller.ts
+++ controllers/client/song.controller.ts
@@ -32,3 +32,29 @@ export const list = async (req: Request, res: Response) => {
         songs: songs,
     });
 }
+
+// [GET] /songs/:slugSong
+export const detail = async (req: Request, res: Response) => {
+    const slugSong: string = req.params.slugSong;
+
+    const song = await Song.findOne({
+        slug: slugSong,
+        deleted: false,
+        status: "active"
+    });
+
+    const topic = await Topic.findOne({
+        _id: song.topicId,
+    }).select("title");
+
+    const singer = await Singer.findOne({
+        _id: song.singerId
+    }).select("fullName");
+
+    res.render("client/pages/songs/detail", {
+        pageTitle: "Chi tiết bài hát",
+        topic: topic,
+        song: song,
+        singer: singer
+    });
+}
